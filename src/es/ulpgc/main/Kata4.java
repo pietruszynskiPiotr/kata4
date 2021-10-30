1cf7114d0a9e8b91b5179117806616af1b07f86c
Piotr Pietruszyński <pietruszynski.piotr@gmail.com>
2021-10-30 13:40:27 +0200

    version 2

--- src/es/ulpgc/main/Kata4.java
+++ src/es/ulpgc/main/Kata4.java
@@ -12,13 +12,28 @@ import java.util.List;
 public class Kata4 {
 
     public static void main(String[] args) throws IOException {
-        MailListReader reader = new MailListReader();
-        List<Mail> mails = reader.read("emails.txt");
-        MailHistogramBuilder mailHistogramBuilder = new MailHistogramBuilder();
-        Histogram<String> histogram = mailHistogramBuilder.build(mails);
+        new Kata4().execute();
+    }
 
+    public void execute() throws IOException {
+        List<Mail> input = input();
+        Histogram<String> process = process(input);
+        output(process);
+    }
+
+    private List<Mail> input() throws IOException {
+        MailListReader reader = new MailListReader();
+        return reader.read("emails.txt");
+    }
+
+    private Histogram<String> process(List<Mail> mails) {
+        MailHistogramBuilder mailHistogramBuilder = new MailHistogramBuilder();
+        return mailHistogramBuilder.build(mails);
+    }
+
+    private void output(Histogram<String> process) {
         java.awt.EventQueue.invokeLater(() -> {
-            new DisplayHistogram("Histogram", histogram).execute();
+            new DisplayHistogram("Histogram", process).execute();
         });
     }
 
